5567573923ed4d101d1b968517ac3f2b2b3fe5da
karasfantom <karasdeveloper@gmail.com>
2024-03-02 14:44:25 +0700

    [update] svg webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    compiler: { emotion: true },
+    reactStrictMode: true,
+    webpack(config) {
+
+        config.module.rules.push(
+            {
+                test: /\.svg$/i,
+                use: [
+                    {
+                        loader: "babel-loader",
+                    },
+                    {
+                        loader: "react-svg-loader",
+                        options: {
+                            jsx: false, // true outputs JSX tags
+                            svgo: {
+                                plugins: [
+                                    { removeViewBox: false },
+                                ],
+                            },
+                        }
+                    },
+
+                ]
+            }
+        );
+        return config;
+    },
+};
 
 export default nextConfig;
