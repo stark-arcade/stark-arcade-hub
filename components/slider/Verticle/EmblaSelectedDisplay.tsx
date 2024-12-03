b865efb18422e6c2017cb6871089eda52c87b5e5
kafierence <vannam34760@gmail.com>
2024-12-03 23:46:38 +0700

    [fix] data

--- components/slider/Verticle/EmblaSelectedDisplay.tsx
+++ components/slider/Verticle/EmblaSelectedDisplay.tsx
@@ -1,13 +1,5 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { EmblaCarouselType } from 'embla-carousel';
-import {
-  Box,
-  Progress,
-  Slider,
-  SliderFilledTrack,
-  SliderThumb,
-  SliderTrack,
-} from '@chakra-ui/react';
+import React, { useCallback, useEffect, useState } from "react";
+import { EmblaCarouselType } from "embla-carousel";
 
 type UseSelectedSnapDisplayType = {
   selectedSnap: number;
@@ -29,8 +21,8 @@ export const useSelectedSnapDisplay = (
     if (!emblaApi) return;
 
     updateScrollSnapState(emblaApi);
-    emblaApi.on('select', updateScrollSnapState);
-    emblaApi.on('reInit', updateScrollSnapState);
+    emblaApi.on("select", updateScrollSnapState);
+    emblaApi.on("reInit", updateScrollSnapState);
   }, [emblaApi, updateScrollSnapState]);
 
   return {
@@ -44,31 +36,23 @@ type PropType = {
   snapCount: number;
 };
 
-export const SelectedSnapDisplay: React.FC<PropType> = props => {
+export const SelectedSnapDisplay: React.FC<PropType> = (props) => {
   const { selectedSnap, snapCount } = props;
 
   return (
-    <Slider
-      aria-label="slider-ex-3"
-      defaultValue={selectedSnap}
-      min={0}
-      value={selectedSnap}
-      step={1}
-      max={snapCount}
-      bg="shader.700"
-      borderRadius="full"
-      orientation="vertical"
-      h="48"
+    <div
+      className="relative h-48 w-8 bg-gray-700 rounded-full flex flex-col-reverse"
+      aria-label="Slider for selecting snap"
     >
-      <SliderTrack
-        bg="shader.500"
-        width="8px"
-        h="fit-content"
-        transform="rotate(-180deg)"
-      >
-        {/* <SliderFilledTrack /> */}
-      </SliderTrack>
-      <SliderThumb bg="gradient.100" h={2} w={2} />
-    </Slider>
+      <div
+        className="absolute w-2 h-2 bg-gradient-to-r from-pink-500 to-purple-500 rounded-full left-1/2 transform -translate-x-1/2 transition-transform"
+        style={{
+          transform: `translate(-50%, calc(${
+            selectedSnap / snapCount
+          } * -100%))`,
+        }}
+      ></div>
+      <div className="flex-grow bg-gray-500 rounded-full"></div>
+    </div>
   );
 };
